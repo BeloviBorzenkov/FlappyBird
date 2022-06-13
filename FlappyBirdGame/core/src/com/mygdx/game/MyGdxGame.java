dc3cac20e4eb7f7a1021282f7e0db848f8aeb485
BeloviBorzenkov <99621923+BeloviBorzenkov@users.noreply.github.com>
2022-06-13 03:57:54 +0300

    Update MyGdxGame.java

--- FlappyBirdGame/core/src/com/mygdx/game/MyGdxGame.java
+++ FlappyBirdGame/core/src/com/mygdx/game/MyGdxGame.java
@@ -5,15 +5,23 @@ import java.util.Iterator;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import java.util.Random;
 
 public class MyGdxGame extends ApplicationAdapter {
+
+	int score;
+
+	int maxscore;
+	BitmapFont font;
+
 	SpriteBatch batch;
 	Background bg;
 	block footer;
@@ -23,15 +31,21 @@ public class MyGdxGame extends ApplicationAdapter {
 	boolean gameOver;
 	Texture restart;
 
+
+
+
 	//метод создания тут создаем все объекты на экране
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-
 		bg = new Background();
 		bird = new Bird();
 		pipes = new Barriers();
 		footer = new block();
+		maxscore= 0;
+		font = new BitmapFont();
+		font.setColor(Color.BLACK);
+		font.getData().setScale(2f, 2f);
 
 		gameOver = false;
 		restart = new Texture("gameover.png");
@@ -45,67 +59,74 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		draw();
 
+
 		if(!gameOver){
 			update();
 		}else{}
 
-		if(Gdx.input.isKeyPressed(Input.Keys.SPACE) && gameOver){
+		if(Gdx.input.isKeyPressed(Input.Keys.ENTER) && gameOver){
 			recreate();
 		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.ALT_LEFT) | gameOver){
-			//вот бы придумать как сделать паузу, нужно остановить метод update, тогда,
-			//напрмер при нажатии на другую кнопку, запускать update заного
+//вот бы придумать как сделать паузу, нужно остановить метод update, тогда,
+//напрмер при нажатии на другую кнопку, запускать update заного
+
 		}
 	}
 
 	//обновление отрисованных объектов, если метод не вызывать картинка застынет
 	public void update(){
 
-			bg.update();
-			bird.update();
-			pipes.update();
+		bg.update();
+		bird.update();
+		pipes.update();
 
-			for (int i = 0; i < Barriers.bar.length; i++) {
-				if(bird.pos.x > Barriers.bar[i].pos.x && bird.pos.x < Barriers.bar[i].pos.x+52){
-					if(!Barriers.bar[i].space.contains(bird.pos)){
-						gameOver = true;
-					}
-				}
-				if(bird.pos.y < 0 || bird.pos.y > 600){
+
+
+		for (int i = 0; i < Barriers.bar.length; i++) {
+			if(bird.pos.x > Barriers.bar[i].pos.x && bird.pos.x < Barriers.bar[i].pos.x+52){
+				if(!Barriers.bar[i].space.contains(bird.pos)){
 					gameOver = true;
 				}
 			}
-
-
-
-
-
-	}
-		//отрисовывает объекты
-		public void draw(){
-			ScreenUtils.clear(1, 1, 1, 1);
-			batch.begin();
-
-			bg.render(batch);
-			pipes.render(batch);
-			bird.render(batch);
-			if(gameOver){
-				batch.draw(restart,300,300);
-
+			if(bird.pos.y < 0 || bird.pos.y > 600){
+				gameOver = true;
 			}
+			if(Barriers.bar[i].space.contains(bird.pos) && bird.pos.x > Barriers.bar[i].pos.x && !gameOver){
+				score++;
+				if (score > maxscore){
+					maxscore = score;
+				}
+			}
+		}
+	}
+	//отрисовывает объекты
+	public void draw(){
+		ScreenUtils.clear(1, 1, 1, 1);
+		batch.begin();
 
+		bg.render(batch);
+		pipes.render(batch);
+		bird.render(batch);
+		if(gameOver){
+			batch.draw(restart,300,300);
 
-			footer.render(batch);
-
-			batch.end();
 		}
 
-		//кидает объекты на стартовые позиции
-		public void recreate(){
-			bird.recreate();
-			pipes.recreate();
-			gameOver = false;
-		}
+		footer.render(batch);
+
+		font.draw(batch, score/15 + "", 400, 550);
+		font.draw(batch, "record: "+maxscore/15, 0,550);
+		batch.end();
+	}
+
+	//кидает объекты на стартовые позиции
+	public void recreate(){
+		bird.recreate();
+		pipes.recreate();
+		gameOver = false;
+		score = 0;
+	}
 
 }
